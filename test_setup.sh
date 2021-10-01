52c24711b7b829263553be1932b387452b1b6d05
James Benson <JamesOBenson@users.noreply.github.com>
2021-10-01 16:10:22 -0500

    Update test_setup.sh
    incorporate other microversion tests

--- test_setup.sh
+++ test_setup.sh
@@ -156,20 +156,20 @@ image_ssh_password = gocubsgo
 image_alt_ssh_password = rebuildPassw0rd
 # ssh_timeout = 60
 
-#[volume]
+[volume]
 #build_timeout = 60
 #backend_names = block
-#min_microversion = $STORAGE_MIN_MICROVERSION
-#max_microversion = $STORAGE_MAX_MICROVERSION
-#volume_size = 1
+min_microversion = $STORAGE_MIN_MICROVERSION
+max_microversion = $STORAGE_MAX_MICROVERSION
+volume_size = 1
 
 [network]
 public_network_id = $PUBLICNETWORKID
 floating_network_name = $PUBLICNETWORKNAME
 
-#[placement]
-#min_microversion = $PLACEMENT_MIN_MICROVERSION
-#max_microversion = $PLACEMENT_MAX_MICROVERSION
+[placement]
+min_microversion = $PLACEMENT_MIN_MICROVERSION
+max_microversion = $PLACEMENT_MAX_MICROVERSION
 
 [heat_plugin]
 minimal_instance_type = 100
